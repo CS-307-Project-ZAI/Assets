f5165e10084fcc31917e277eb921c7817b2b7b1a
Jacob Dunbar <dunbarj@purdue.edu>
2017-01-26 16:32:55 -0500

    Added files I forgot earlier..

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public int health = 50;
+	public float moveSpeed = 0.5f;
+	public PlayerController target;
+	public float spawnTime = 5.0f;
+	public int spawnAmount = 1;
+	public EnemyController spawn;
+
+	float timer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getMovement ();
+		getRotation ();
+		checkSpawnTime ();
+	}
+
+	void getMovement() {
+		float dirX = target.transform.position.x - transform.position.x;
+		float dirY = target.transform.position.y - transform.position.y;
+		Vector3 dir = new Vector3(dirX, dirY, 0);
+		transform.position += Vector3.ClampMagnitude(dir, moveSpeed) * Time.deltaTime;
+	}
+
+	void getRotation() {
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, target.transform.position - transform.position);
+		transform.Rotate (new Vector3 (0, 0, 45.0f));
+	}
+
+	void checkSpawnTime() {
+		timer += Time.deltaTime;
+		if (timer > spawnTime) {
+			for (int i = 0; i < spawnAmount; i++) {
+				spawnChild ();
+			}
+			timer = 0.0f;
+		}
+	}
+
+	void spawnChild() {
+		EnemyController e = (EnemyController) Instantiate (spawn);
+		e.target = target;
+		e.transform.position = transform.position + new Vector3(1, 0, 0);
+		e.spawn = spawn;
+		e.timer = 0.0f;
+	}
+
+	public void aliveCheck() {
+		if (health <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	void ApplyDamage(int dmg) {
+		this.health -= dmg;
+		aliveCheck ();
+	}
+}
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public PlayerController player;
+	public List<EnemyController> enemies;
+	public EnemyController enemy;
+	public int spawnAmount = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			for (int i = 0; i < spawnAmount; i++) {
+				spawnEnemy ();
+			}
+		}
+	}
+
+	void spawnEnemy() {
+		EnemyController e = (EnemyController) Instantiate (enemy);
+		e.target = player;
+		e.transform.position = new Vector3 (0, 0, 0);
+		e.spawn = enemy;
+		enemies.Add (e);
+	}
+}
