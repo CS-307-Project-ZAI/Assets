93d7b956a23b953f71ce03c3ef29a19c751dfd4c
Jacob Dunbar <dunbarj@purdue.edu>
2017-02-13 23:54:35 -0500

    GUI and Command Mode Additions
    -Added more to the bottom-right GUI. Now the GUI on the bottom-right
    cycles with the mode.
    -User can now select Allies while in Command mode, but cannot yet give
    commands.

--- Scripts/UIController.cs
+++ Scripts/UIController.cs
@@ -8,35 +8,100 @@ public class UIController : MonoBehaviour {
 	GameManager gm;
 
 	//GameObject infoBoxLeft;
-	//GameObject infoBoxRight;
+	GameObject combatBoxRight;
+	GameObject commandBoxRight;
+	GameObject buildBoxRight;
 	RectTransform healthBar;
 
 	[HideInInspector]
-	public Text modeText;
-	public Text ammoPool;
-	public Text ammoLeft;
+	//Left info box
+	Text modeText;
+
+	//Combat box right
+	Text weapon;
+	Text ammoPool;
+	Text ammoLeft;
+
+	//Command box right
+	Text selected;
+
+	//Health bar
 	public Text playerHealth;
+
+
+	private string activeGUI = "";
 
 	public void GMStart() {
 		gm = FindObjectOfType<GameManager> ();
 		//infoBoxLeft = transform.Find ("Info Box Left").gameObject;
-		//infoBoxRight = transform.Find ("Info Box Right").gameObject;
 		modeText = GameObject.Find ("Mode").GetComponent<Text>();
-		ammoPool = GameObject.Find ("AmmoPool").GetComponent<Text> ();
-		ammoLeft = GameObject.Find ("AmmoLeft").GetComponent<Text> ();
+
+		//Combat box right
+		combatBoxRight = transform.Find ("Combat Box Right").gameObject;
+		weapon = combatBoxRight.transform.Find ("Weapon").GetComponent<Text>();
+		ammoPool = combatBoxRight.transform.Find ("AmmoPool").GetComponent<Text> ();
+		ammoLeft = combatBoxRight.transform.Find ("AmmoLeft").GetComponent<Text> ();
+
+		//Command box right
+		commandBoxRight = transform.Find ("Command Box Right").gameObject;
+		selected = commandBoxRight.transform.Find ("Selected").GetComponent<Text> ();
+
+		//Build box right
+		buildBoxRight = transform.Find ("Build Box Right").gameObject;
+
+		//Health bar
 		playerHealth = GameObject.Find ("Health Amount").GetComponent<Text> ();
 		healthBar = GameObject.Find ("Health Bar").GetComponent<RectTransform> ();
 	}
 
 	public void GMUpdate() {
-		ammoLeft.text = gm.player.weapons [gm.player.currentWeapon].currentLoaded.ToString ();
-		if (gm.player.weapons [gm.player.currentWeapon].ammoPool == -1) {
-			ammoPool.text = "∞";
-		} else {
-			ammoPool.text = gm.player.weapons [gm.player.currentWeapon].ammoPool.ToString ();
+		if (activeGUI != gm.playerMode) {
+			activeGUI = gm.playerMode;
+			switch (activeGUI) {
+			case "Combat":
+				combatBoxRight.SetActive (true);
+				commandBoxRight.SetActive (false);
+				buildBoxRight.SetActive (false);
+				break;
+			case "Command":
+				combatBoxRight.SetActive (false);
+				commandBoxRight.SetActive (true);
+				buildBoxRight.SetActive (false);
+				break;
+			case "Build":
+				combatBoxRight.SetActive (false);
+				commandBoxRight.SetActive (false);
+				buildBoxRight.SetActive (true);
+				break;
+			}
 		}
+
+		modeText.text = activeGUI;
+
+		switch (activeGUI) {
+		case "Combat":
+			weapon.text = gm.player.weapons [gm.player.currentWeapon].weaponName;
+			ammoLeft.text = gm.player.weapons [gm.player.currentWeapon].currentLoaded.ToString ();
+			if (gm.player.weapons [gm.player.currentWeapon].ammoPool == -1) {
+				ammoPool.text = "∞";
+			} else {
+				ammoPool.text = gm.player.weapons [gm.player.currentWeapon].ammoPool.ToString ();
+			}
+			break;
+		case "Command":
+			if (gm.selectedAlly != null) {
+				selected.text = gm.selectedAlly.name;
+			} else {
+				selected.text = "None";
+			}
+			break;
+		case "Build":
+			break;
+		}
+
+		//Update Health Bar
 		playerHealth.text = gm.player.health.ToString();
-		float width = ((float)gm.player.health / 50.0f) * 260;
+		float width = ((float)gm.player.health / 50.0f) * 290;
 		if (width < 0) {
 			width = 0.0f;
 		}
