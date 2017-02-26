cc1e03a6bcd4b52cafc7b27d33500566c974454f
jone1268 <jone1268@purdue.edu>
2017-02-26 18:41:52 -0500

    Merge commit...hopefully this works

---
+++ Scripts/Wall.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour {
+
+	public Wall wall;
+	public string wallTier;
+	public int wallHealth;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void ApplyDamage(int dmg){
+		this.wallHealth -= dmg;
+		if (this.wallHealth < 0) {
+			this.wallHealth = 0;
+		}
+		wallHealthCheck ();
+	}
+
+	public virtual void wallHealthCheck(){
+		if (wallHealth <= 0) {
+			//Destroy Wall GameObject
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
