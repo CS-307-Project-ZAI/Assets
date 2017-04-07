29781af385adbbe1daf8f2cfa6b238e43dae3a89
jone1268 <jone1268@purdue.edu>
2017-04-06 20:48:32 -0400

    Adding Item.cs

---
+++ Scripts/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	public GameManager gm;
+
+	public string itemType;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			Debug.Log ("Person came within range of item!");
+			PersonController temp = other.transform.gameObject.GetComponent<PersonController> ();
+			gm.player.playerInventory [itemType]++;
+			Destroy (this.gameObject);
+		}
+	}
+
+
+}
